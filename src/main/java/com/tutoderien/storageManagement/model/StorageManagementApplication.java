a15fac47c62648a10f6ebe083fac3da65018d608
Georges Paulin <paulpaul047@yahoo.com>
2020-11-21 18:01:48 +0100

    first projet

---
+++ src/main/java/com/tutoderien/storageManagement/model/StorageManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tutoderien.storageManagement.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutoderien/storageManagement/model/StorageManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoderien.storageManagement.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
